8189446f1cd2e0470d33ee0fdb90e5a000df60a9
Sriprasath <sriprasath.cptech@gmail.com>
2022-01-23 22:27:20 +0530

    create vet index page and controller. closes #13

---
+++ sf-pet-clinic-web/src/main/java/com/sf/sfpetclinic/controllers/VetsController.java
@@ -0,0 +1,12 @@
+package com.sf.sfpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetsController {
+    @RequestMapping("/vets")
+    public String listVets() {
+        return "vets/index";
+    }
+}
