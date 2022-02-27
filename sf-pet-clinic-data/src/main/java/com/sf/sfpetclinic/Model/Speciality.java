c3187f64b3d0e87e8c24d66211569cb96ef8772f
Sriprasath <sriprasath.cptech@gmail.com>
2022-02-27 15:36:38 +0530

    convert vet to jpa entity. closes #35

--- sf-pet-clinic-data/src/main/java/com/sf/sfpetclinic/Model/Speciality.java
+++ sf-pet-clinic-data/src/main/java/com/sf/sfpetclinic/Model/Speciality.java
@@ -1,6 +1,13 @@
 package com.sf.sfpetclinic.Model;
 
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "specialties")
 public class Speciality extends BaseEntity {
+
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
