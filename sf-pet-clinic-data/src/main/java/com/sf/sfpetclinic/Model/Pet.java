4dd9286762f7d29dd07069ccd86ff6200c13d91e
Sriprasath <sriprasath.cptech@gmail.com>
2022-02-27 15:19:41 +0530

    converts owners to jpa entities.. closes #34

--- sf-pet-clinic-data/src/main/java/com/sf/sfpetclinic/Model/Pet.java
+++ sf-pet-clinic-data/src/main/java/com/sf/sfpetclinic/Model/Pet.java
@@ -1,17 +1,48 @@
 package com.sf.sfpetclinic.Model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.Date;
 
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity{
-    private LocalDate birthDate;
-    private Owner owner;
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petType;
 
-    public LocalDate getBirthDate() {
+    @Column(name = "birth_date")
+    private Date birthDate;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private Owner owner;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public PetType getPetType() {
+        return petType;
+    }
+
+    public void setPetType(PetType petType) {
+        this.petType = petType;
+    }
+
+    public Date getBirthDate() {
         return birthDate;
     }
 
-    public void setBirthDate(LocalDate birthDate) {
+    public void setBirthDate(Date birthDate) {
         this.birthDate = birthDate;
     }
 
@@ -21,13 +52,5 @@ public class Pet extends BaseEntity{
 
     public void setOwner(Owner owner) {
         this.owner = owner;
-    }
-
-    public PetType getPetType() {
-        return petType;
-    }
-
-    public void setPetType(PetType petType) {
-        this.petType = petType;
     }
 }
