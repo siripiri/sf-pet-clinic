40c92a8892e5b017590d6788de8acbb5ba7e8298
Sriprasath <sriprasath.cptech@gmail.com>
2022-03-01 22:16:44 +0530

    added SDJ for vet. closes #39

---
+++ sf-pet-clinic-data/src/main/java/com/sf/sfpetclinic/service/springdatajpa/VetSDJservice.java
@@ -0,0 +1,51 @@
+package com.sf.sfpetclinic.service.springdatajpa;
+
+import com.sf.sfpetclinic.Model.Speciality;
+import com.sf.sfpetclinic.Model.Vet;
+import com.sf.sfpetclinic.repository.SpecialityRespository;
+import com.sf.sfpetclinic.repository.VetRepository;
+import com.sf.sfpetclinic.service.VetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springDataJPA")
+public class VetSDJservice implements VetService {
+    private final VetRepository vetRepository;
+    private final SpecialityRespository specialityRespository;
+
+    public VetSDJservice(VetRepository vetRepository, SpecialityRespository specialityRespository) {
+        this.vetRepository = vetRepository;
+        this.specialityRespository = specialityRespository;
+    }
+
+    @Override
+    public Vet findById(Long aLong) {
+        return vetRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return vetRepository.save(object);
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        Set<Vet> vets = new HashSet<>();
+        vetRepository.findAll().forEach(vets::add);
+        return vets;
+    }
+
+    @Override
+    public void delete(Vet object) {
+        vetRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        vetRepository.deleteById(aLong);
+    }
+}
