d7b46b3fff56e17748f9eb97cce780eccb4a05e2
Sriprasath <sriprasath.cptech@gmail.com>
2022-03-22 14:47:46 +0530

    CRUD tests for owner map service. closes #47

---
+++ sf-pet-clinic-data/src/test/java/com/sf/sfpetclinic/service/map/OwnerMapServiceTest.java
@@ -0,0 +1,70 @@
+package com.sf.sfpetclinic.service.map;
+
+import com.sf.sfpetclinic.Model.Owner;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OwnerMapServiceTest {
+
+    OwnerMapService ownerMapService;
+    final Long ownerId = 1L;
+    final String lastName = "Smith";
+    @BeforeEach
+    void setUp() {
+        ownerMapService = new OwnerMapService(new PetTypeMapService(), new PetMapService());
+        Owner owner = new Owner();
+        owner.setId(1L);
+        owner.setLastname(lastName);
+        ownerMapService.save(owner);
+    }
+
+    @Test
+    void findByLastname() {
+        Owner searchResult = ownerMapService.findByLastname("foo");
+        assertNull(searchResult);
+    }
+
+    @Test
+    void findById() {
+        Owner owner = ownerMapService.findById(1L);
+        assertEquals(ownerId, owner.getId());
+    }
+
+    @Test
+    void saveExistingId() {
+        Long id = 2L;
+        Owner owner2 = new Owner();
+        owner2.setId(id);
+        Owner savedOwner = ownerMapService.save(owner2);
+        assertEquals(id, savedOwner.getId());
+    }
+
+    @Test
+    void saveNoId() {
+        Owner savedOwner = ownerMapService.save(new Owner());
+        assertNotNull(savedOwner);
+        assertNotNull(savedOwner.getId());
+    }
+
+    @Test
+    void findAll() {
+        Set<Owner> ownerSet = ownerMapService.findAll();
+        assertEquals(1,ownerSet.size());
+    }
+
+    @Test
+    void delete() {
+        ownerMapService.delete(ownerMapService.findById(ownerId));
+        assertEquals(0,ownerMapService.findAll().size());
+    }
+
+    @Test
+    void deleteById() {
+        ownerMapService.deleteById(ownerId);
+        assertEquals(0,ownerMapService.findAll().size());
+    }
+}
